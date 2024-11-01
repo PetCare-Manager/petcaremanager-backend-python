d78cd722779ec67fb5c63824a643765c79274bb3
Emilio Sala <emilio_k88@hotmail.com>
2024-11-01 10:03:49 +0100

    jwt for authentication

---
+++ middlewares/jwt_bearer.py
@@ -0,0 +1,36 @@
+"""
+JWTBearer class for handling authentication using JSON Web Tokens (JWT).
+
+This class extends FastAPI's HTTPBearer to implement token-based authentication.
+It verifies the presence and validity of a JWT token in the Authorization header
+of incoming requests.
+"""
+from fastapi import HTTPException, Request
+from fastapi.security import HTTPBearer
+from utils.jwt_manager import validate_token
+
+
+class JWTBearer(HTTPBearer):
+    """
+    Methods:
+        __call__: Overrides the parent class method to validate the JWT token.
+            Raises an HTTPException if the token is invalid, expired, or missing.
+
+    Usage:
+        Use JWTBearer as a dependency in FastAPI routes to protect endpoints that
+        require authentication. The token must be provided in the format:
+        "Authorization: Bearer <token>".
+
+    Raises:
+        HTTPException: If the token is missing, invalid, or expired, with a status code of 403.
+    """
+    async def __call__(self, request: Request):
+        auth = await super().__call__(request)
+        if not auth or not auth.credentials:
+            raise HTTPException(status_code=403, detail="Invalid or missing credentials")
+        try:
+            data = validate_token(auth.credentials)
+            if not data:
+                raise HTTPException(status_code=403, detail="Invalid or expired token")
+        except Exception as e:
+            raise HTTPException(status_code=403, detail=str(e)) from e
