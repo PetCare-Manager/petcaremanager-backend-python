0fa5d8ea418a3850b411eea14e243a92739dbf11
Emilio Sala <emilio_k88@hotmail.com>
2024-11-01 06:17:22 +0100

    user interaction with database CRUD

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ PetCareManager Backend built FastAPI
 """
 
 from fastapi import FastAPI
+from config.database import engine, Base
+from routers.user import user_router
+
 
 app = FastAPI(
     title="PetCare Manager",
     description="Manage your pets schedules, appointments, medicine",
     version="0.0.1",
 )
+app.include_router(user_router, prefix="/api")
 
 @app.get("/")
 def read_root():
@@ -19,3 +23,5 @@ def read_root():
 def health_check():
     """Health Check"""
     return {"status": "ok"}
+
+Base.metadata.create_all(bind=engine)
---
+++ services/user.py
@@ -0,0 +1,58 @@
+""""
+Implementation of the User service to interact with the database.
+"""
+# from typing import List
+from typing import Optional
+
+from sqlalchemy.orm import Session
+from models.user import User as UserModel
+from schemas.user import UserLogin, UserUpdate
+
+
+class UserService:
+    """
+    UserService provides methods to interact with the User table in the database.
+    """
+
+    def __init__(self, db: Session) -> None:
+        self.db = db
+
+    def get_user_by_id(self, _id: int) -> Optional[UserModel]:
+        """Get user by given id."""
+        return self.db.query(UserModel).filter(UserModel.id == _id).first()
+
+    def get_user_by_email(self, email: str) -> Optional[UserModel]:
+        """Get user by given email."""
+        return self.db.query(UserModel).filter(UserModel.email == email).first()
+
+    def create_user(self, user: UserLogin) -> UserModel:
+        """Create a new user in the database"""
+        try:
+            new_user = UserModel(**vars(user))
+            self.db.add(new_user)
+            self.db.commit()
+            self.db.refresh(new_user)
+            return new_user
+        except Exception as e:
+            self.db.rollback()  
+            raise RuntimeError(f"An error occurred while creating the user: {e}") from e
+
+    def update_user(self, _id: int, user_update: UserUpdate) ->  Optional[UserModel]:
+        """Update username by given id."""
+        user = self.get_user_by_id(_id)
+        if not user:
+            return None
+        if user.username is not None:
+            user.username = user_update.username # type: ignore
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+
+    def delete_user(self, _id: int) -> None:
+        """Delete user by given id."""
+        user = self.get_user_by_id(_id)
+        if not user:
+            return None
+        self.db.delete(user)
+        self.db.commit()
+        return None
