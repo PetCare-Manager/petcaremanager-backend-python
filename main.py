58ee6c760a459625190c7cf158386aad0d3a0bf1
Emilio Sala <emilio_k88@hotmail.com>
2024-11-01 16:51:20 +0100

    adding error_handler

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ PetCareManager Backend built FastAPI
 """
 
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
 from config.database import engine, Base
 from middlewares.cors import setup_cors
+from middlewares.error_handler import ErrorHandler
 from routers.user import user_router
 from routers.password import pass_router
 
@@ -15,6 +17,7 @@ app = FastAPI(
     version="0.0.1",
 )
 setup_cors(app)
+app.add_middleware(BaseHTTPMiddleware, dispatch=ErrorHandler(app).dispatch)
 
 app.include_router(user_router, prefix="/api")
 app.include_router(pass_router, prefix="/api/auth/password", tags=["Auth"])
