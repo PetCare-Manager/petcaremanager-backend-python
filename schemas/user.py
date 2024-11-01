de5e408c6708f9e8db0013d5bf60733948a0be2a
Emilio Sala <emilio_k88@hotmail.com>
2024-11-01 04:51:24 +0100

    define schemas User with pydantic

---
+++ schemas/user.py
@@ -0,0 +1,29 @@
+"""
+Definition of schemas for User
+"""
+from datetime import datetime
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    """Base schema for a user with the essential attribute email."""
+    email: str
+
+class UserLogin(UserBase):
+    """Schema for user login, includes email and password."""
+    password: str
+
+class User(UserBase):
+    """Schema for returning user information, includes id."""
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class UserInfo(User):
+    """Detailed schema for returning user information, includes username and created_at."""
+    username: str
+    created_at: datetime
+
+class UserUpdate(BaseModel):
+    """Schema for updating user information, currently only allows updating the username."""
+    username: str
