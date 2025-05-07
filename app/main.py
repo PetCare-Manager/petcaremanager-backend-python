e88d87ab0c6b1f96a390c8ad068ed26ee80319a0
Diego Guaman <diegoguamanmedina@gmail.com>
2025-05-07 21:42:13 +0200

    borrar espacios en blanco

--- app/main.py
+++ app/main.py
@@ -29,7 +29,6 @@ app.include_router(user_router, prefix="/api")
 app.include_router(pass_router, prefix="/api/auth/password", tags=["Auth"])
 app.include_router(pet_router, prefix="/api/pets", tags=["Pets"])
 
-
 @app.get("/health", tags=["Health"])
 def health_check():
     """Health Check"""
