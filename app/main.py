1af1ed8dc783fe1326faa111d29d723fbb299091
Diego Guaman <diegoguamanmedina@gmail.com>
2025-05-07 21:40:20 +0200

    borrar linea de main

--- app/main.py
+++ app/main.py
@@ -28,7 +28,6 @@ app.add_middleware(ErrorHandler)
 app.include_router(user_router, prefix="/api")
 app.include_router(pass_router, prefix="/api/auth/password", tags=["Auth"])
 app.include_router(pet_router, prefix="/api/pets", tags=["Pets"])
-app.include_router(pet_router, prefix="/api/pets", tags=["Pets"])
 
 
 @app.get("/health", tags=["Health"])
