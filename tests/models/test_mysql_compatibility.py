6ea2a6e1f5d2418bb8d65b2a89b6b31942b5e5ab
MadelayHeily-hub <madcodlife@gmail.com>
2025-07-26 17:19:27 +0200

    Testear los modelos antes de la migración

---
+++ tests/models/test_mysql_compatibility.py
@@ -0,0 +1,8 @@
+def test_string_lengths_defined():
+    """Verificar que todos los String tengan longitud para MySQL"""
+    
+def test_password_field_length():
+    """Verificar que password soporte hashes bcrypt (60+ chars)"""
+    
+def test_enum_compatibility():
+    """Verificar que los enums funcionen en MySQL"""
